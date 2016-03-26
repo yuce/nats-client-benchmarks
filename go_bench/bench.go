1aae42fb70165af4e3100d44b48ef0e0a5c00b32
Yuce Tekol <yucetekol@gmail.com>
2016-03-26 18:42:21 +0200

    go bench: Renamed numMsgs to msgCount

--- go_bench/bench.go
+++ go_bench/bench.go
@@ -18,7 +18,7 @@ const subj = "0123456789012345"
 const payload = "0123456789012345012345678901234501234567890123450123456789012345"
 const numPubs = 1
 const numSubs = 1
-const numMsgs = 1000000
+const MsgCount = 1000000
 
 func main() {
 	opts := nats.DefaultOptions
@@ -34,28 +34,28 @@ func main() {
 	// Run Subscribers first
 	startwg.Add(numSubs)
 	for i := 0; i < numSubs; i++ {
-		go runSubscriber(&startwg, &donewg, opts, numMsgs * numPubs)
+		go runSubscriber(&startwg, &donewg, opts, MsgCount * numPubs)
 	}
 	startwg.Wait()
 
 	// Now Publishers
 	startwg.Add(numPubs)
 	for i := 0; i < numPubs; i++ {
-		go runPublisher(&startwg, &donewg, opts, numMsgs)
+		go runPublisher(&startwg, &donewg, opts, MsgCount)
 	}
 
 	startwg.Wait()
 
 	donewg.Wait()
 	delta := time.Since(start).Seconds()
-	total := float64(numMsgs * numPubs)
+	total := float64(MsgCount * numPubs)
 	if numSubs > 0 {
 		total *= float64(numSubs)
 	}
     fmt.Printf("%d %d %d\n", int64(total), int64(delta * 1000000), int64(total/delta))
 }
 
-func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs int) {
+func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, MsgCount int) {
 	nc, err := opts.Connect()
 	if err != nil {
 		log.Fatalf("Can't connect: %v\n", err)
@@ -63,14 +63,14 @@ func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs in
 	defer nc.Close()
 	startwg.Done()
 
-	for i := 0; i < numMsgs; i++ {
+	for i := 0; i < MsgCount; i++ {
 		nc.Publish(subj, []byte(payload))
 	}
 	nc.Flush()
 	donewg.Done()
 }
 
-func runSubscriber(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs int) {
+func runSubscriber(startwg, donewg *sync.WaitGroup, opts nats.Options, MsgCount int) {
 	nc, err := opts.Connect()
 	if err != nil {
 		log.Fatalf("Can't connect: %v\n", err)
@@ -79,7 +79,7 @@ func runSubscriber(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs i
 	received := 0
 	nc.Subscribe(subj, func(msg *nats.Msg) {
 		received++
-		if received >= numMsgs {
+		if received >= MsgCount {
 			donewg.Done()
 			nc.Close()
 		}
